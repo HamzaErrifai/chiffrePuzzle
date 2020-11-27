bfb13805815dab90d20a3a336be539e325e26e79
Hamza Errifai <hamzaerrifai@hotmail.com>
2020-11-27 13:35:46 +0100

    Ajoutez des fichiers projet.

---
+++ chiffrePuzzle/Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿
+namespace chiffrePuzzle
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.FlatAppearance.BorderColor = System.Drawing.Color.Teal;
+            this.btn1.FlatAppearance.BorderSize = 2;
+            this.btn1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn1.Location = new System.Drawing.Point(21, 187);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(113, 96);
+            this.btn1.TabIndex = 0;
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.btnPuzzleKeyDown);
+            this.btn1.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.btnPuzzlePreviewKeyDown);
+            // 
+            // btn2
+            // 
+            this.btn2.FlatAppearance.BorderColor = System.Drawing.Color.Teal;
+            this.btn2.FlatAppearance.BorderSize = 2;
+            this.btn2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn2.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn2.Location = new System.Drawing.Point(159, 187);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(113, 96);
+            this.btn2.TabIndex = 1;
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.btnPuzzleKeyDown);
+            this.btn2.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.btnPuzzlePreviewKeyDown);
+            // 
+            // btn3
+            // 
+            this.btn3.FlatAppearance.BorderColor = System.Drawing.Color.Teal;
+            this.btn3.FlatAppearance.BorderSize = 2;
+            this.btn3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn3.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn3.Location = new System.Drawing.Point(297, 187);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(113, 96);
+            this.btn3.TabIndex = 2;
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.KeyDown += new System.Windows.Forms.KeyEventHandler(this.btnPuzzleKeyDown);
+            this.btn3.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.btnPuzzlePreviewKeyDown);
+            // 
+            // btn4
+            // 
+            this.btn4.FlatAppearance.BorderColor = System.Drawing.Color.Teal;
+            this.btn4.FlatAppearance.BorderSize = 2;
+            this.btn4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn4.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn4.Location = new System.Drawing.Point(21, 325);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(113, 96);
+            this.btn4.TabIndex = 3;
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.KeyDown += new System.Windows.Forms.KeyEventHandler(this.btnPuzzleKeyDown);
+            this.btn4.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.btnPuzzlePreviewKeyDown);
+            // 
+            // btn5
+            // 
+            this.btn5.FlatAppearance.BorderColor = System.Drawing.Color.Teal;
+            this.btn5.FlatAppearance.BorderSize = 2;
+            this.btn5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn5.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn5.Location = new System.Drawing.Point(159, 325);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(113, 96);
+            this.btn5.TabIndex = 4;
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.KeyDown += new System.Windows.Forms.KeyEventHandler(this.btnPuzzleKeyDown);
+            this.btn5.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.btnPuzzlePreviewKeyDown);
+            // 
+            // btn6
+            // 
+            this.btn6.FlatAppearance.BorderColor = System.Drawing.Color.Teal;
+            this.btn6.FlatAppearance.BorderSize = 2;
+            this.btn6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn6.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn6.Location = new System.Drawing.Point(297, 325);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(113, 96);
+            this.btn6.TabIndex = 5;
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.KeyDown += new System.Windows.Forms.KeyEventHandler(this.btnPuzzleKeyDown);
+            this.btn6.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.btnPuzzlePreviewKeyDown);
+            // 
+            // btn7
+            // 
+            this.btn7.FlatAppearance.BorderColor = System.Drawing.Color.Teal;
+            this.btn7.FlatAppearance.BorderSize = 2;
+            this.btn7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn7.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn7.Location = new System.Drawing.Point(21, 463);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(113, 96);
+            this.btn7.TabIndex = 6;
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.KeyDown += new System.Windows.Forms.KeyEventHandler(this.btnPuzzleKeyDown);
+            this.btn7.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.btnPuzzlePreviewKeyDown);
+            // 
+            // btn8
+            // 
+            this.btn8.FlatAppearance.BorderColor = System.Drawing.Color.Teal;
+            this.btn8.FlatAppearance.BorderSize = 2;
+            this.btn8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn8.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn8.Location = new System.Drawing.Point(159, 463);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(113, 96);
+            this.btn8.TabIndex = 7;
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.KeyDown += new System.Windows.Forms.KeyEventHandler(this.btnPuzzleKeyDown);
+            this.btn8.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.btnPuzzlePreviewKeyDown);
+            // 
+            // btn9
+            // 
+            this.btn9.FlatAppearance.BorderColor = System.Drawing.Color.Teal;
+            this.btn9.FlatAppearance.BorderSize = 2;
+            this.btn9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn9.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn9.Location = new System.Drawing.Point(297, 463);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(113, 96);
+            this.btn9.TabIndex = 8;
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.KeyDown += new System.Windows.Forms.KeyEventHandler(this.btnPuzzleKeyDown);
+            this.btn9.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.btnPuzzlePreviewKeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(21, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(239, 37);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Puzzle digits game";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnStart.Location = new System.Drawing.Point(303, 19);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(118, 37);
+            this.btnStart.TabIndex = 10;
+            this.btnStart.Text = "StartGame";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.AutoSize = true;
+            this.lblTimer.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblTimer.Location = new System.Drawing.Point(177, 89);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(83, 37);
+            this.lblTimer.TabIndex = 11;
+            this.lblTimer.Text = "00:00";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.panel1.Controls.Add(this.lblStatus);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.lblTimer);
+            this.panel1.Location = new System.Drawing.Point(0, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(442, 154);
+            this.panel1.TabIndex = 12;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblStatus.ForeColor = System.Drawing.Color.Navy;
+            this.lblStatus.Location = new System.Drawing.Point(226, 72);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 21);
+            this.lblStatus.TabIndex = 13;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(159, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 17);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Time Left";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.Highlight;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(126, 581);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(284, 37);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Made By Hamza Errifai";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(442, 640);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblTimer;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ chiffrePuzzle/Form1.cs
@@ -0,0 +1,359 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace chiffrePuzzle
+{
+    public partial class Form1 : Form
+    {
+        private bool gameStarted;
+        private bool gamePaused;
+        private int count = 0;
+        private int counter;
+        private List<Button> listButtons;
+        private List<string> currentListNumbers;
+
+        public Form1()
+        {
+            InitializeComponent();
+            gameStarted = false;
+            gamePaused = false;
+            listButtons = new List<Button> { btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9 };
+            // center the form
+            this.CenterToScreen();
+            this.MaximizeBox = false; //remove the maximize button
+            this.MinimizeBox = false; //remove the minimize button
+            this.FormBorderStyle = FormBorderStyle.FixedSingle; //fixed window size
+        }
+
+        private List<string> GetRandomNumbers()
+        {
+            string[] numbers = { "1", "2", "3", "4", "5", "6", "7", "8", "" };
+            Random random = new Random();
+            List<string> randomNumbers = new List<string>();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int number;
+                do
+                {
+                    number = random.Next(0, numbers.Length);
+                }
+                while (randomNumbers.Contains(numbers[number]));
+
+                randomNumbers.Add(numbers[number]);
+            }
+
+            return randomNumbers;
+        }
+
+        private void hideButtonsText()
+        {
+            List<string> emptyList = new List<string>(Enumerable.Repeat("", listButtons.Count));
+            setButtonsText(emptyList);
+        }
+
+        private void pauseGame()
+        {
+            if (!gamePaused)
+            {
+                saveCurrentNumbers();
+                hideButtonsText();
+                gamePaused = true;
+                btnStart.Text = "Continue game";
+                // pause the timer
+                timer1.Stop();
+                lblStatus.Text = "Paused";
+
+            }
+            else //continue the game
+            {
+                gamePaused = false;
+                setButtonsText(currentListNumbers);
+                btnStart.Text = "Pause game";
+                // resume the timer
+                timer1.Start();
+                lblStatus.Text = "";
+            }
+        }
+
+        private void startTimer()
+        {
+            timer1 = new System.Windows.Forms.Timer();
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer1.Interval = 1000; // 1 second
+            TimeSpan t = TimeSpan.FromSeconds(counter);
+            timer1.Start();
+            gameStarted = true;
+            lblTimer.Text = (t).ToString(@"mm\:ss");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            counter--;
+            if (counter == 0)
+            {
+
+                timer1.Dispose();
+                MessageBox.Show("Time is up!! Game over");
+                btnStart.Text = "Restart";
+                gameStarted = false;
+            }
+            TimeSpan t = TimeSpan.FromSeconds(counter);
+            lblTimer.Text = t.ToString(@"mm\:ss");
+
+        }
+
+        private void setButtonsText(List<string> nNbs)
+        {
+            for (int i = 0; i < listButtons.Count; i++)
+            {
+                listButtons[i].Text = nNbs[i];
+            }
+        }
+
+        private void saveCurrentNumbers()
+        {
+            for (int i = 0; i < listButtons.Count; i++)
+            {
+                currentListNumbers[i] = listButtons[i].Text;
+            }
+        }
+
+        private void generateNumbers()
+        {
+            currentListNumbers = GetRandomNumbers();
+            setButtonsText(currentListNumbers);
+        }
+
+        private void downAction()
+        {
+            count = 1;
+
+            if (btn4.Text == "" && count == 1)
+            {
+                btn4.Text = btn1.Text;
+                btn1.Text = "";
+                count = count + 1;
+            }
+            if (btn5.Text == "" && count == 1)
+            {
+                btn5.Text = btn2.Text;
+                btn2.Text = "";
+                count = count + 1;
+            }
+            if (btn6.Text == "" && count == 1)
+            {
+                btn6.Text = btn3.Text;
+                btn3.Text = "";
+                count = count + 1;
+            }
+            if (btn7.Text == "" && count == 1)
+            {
+                btn7.Text = btn4.Text;
+                btn4.Text = "";
+                count = count + 1;
+            }
+            if (btn8.Text == "" && count == 1)
+            {
+                btn8.Text = btn5.Text;
+                btn5.Text = "";
+                count = count + 1;
+            }
+            if (btn9.Text == "" && count == 1)
+            {
+                btn9.Text = btn6.Text;
+                btn6.Text = "";
+                count = count + 1;
+            }
+            win();
+        }
+
+        private void upAction()
+        {
+            count = 1;
+            if (btn1.Text == "" && count == 1)
+            {
+                btn1.Text = btn4.Text;
+                btn4.Text = "";
+                count = count + 1;
+            }
+            if (btn2.Text == "" && count == 1)
+            {
+                btn2.Text = btn5.Text;
+                btn5.Text = "";
+                count = count + 1;
+            }
+            if (btn3.Text == "" && count == 1)
+            {
+                btn3.Text = btn6.Text;
+                btn6.Text = "";
+                count = count + 1;
+            }
+            if (btn4.Text == "" && count == 1)
+            {
+                btn4.Text = btn7.Text;
+                btn7.Text = "";
+                count = count + 1;
+            }
+            if (btn5.Text == "" && count == 1)
+            {
+                btn5.Text = btn8.Text;
+                btn8.Text = "";
+                count = count + 1;
+            }
+            if (btn6.Text == "" && count == 1)
+            {
+                btn6.Text = btn9.Text;
+                btn9.Text = "";
+                count = count + 1;
+            }
+            win();
+        }
+
+        private void rightAction()
+        {
+            count = 1;
+            if (btn2.Text == "" && count == 1)
+            {
+                btn2.Text = btn1.Text;
+                btn1.Text = "";
+                count = count + 1;
+            }
+            if (btn5.Text == "" && count == 1)
+            {
+                btn5.Text = btn4.Text;
+                btn4.Text = "";
+                count = count + 1;
+            }
+            if (btn8.Text == "" && count == 1)
+            {
+                btn8.Text = btn7.Text;
+                btn7.Text = "";
+                count = count + 1;
+            }
+            if (btn3.Text == "" && count == 1)
+            {
+                btn3.Text = btn2.Text;
+                btn2.Text = "";
+                count = count + 1;
+            }
+            if (btn6.Text == "" && count == 1)
+            {
+                btn6.Text = btn5.Text;
+                btn5.Text = "";
+                count = count + 1;
+            }
+            if (btn9.Text == "" && count == 1)
+            {
+                btn9.Text = btn8.Text;
+                btn8.Text = "";
+                count = count + 1;
+            }
+            win();
+        }
+
+        private void leftAction()
+        {
+            count = 1;
+            if (btn1.Text == "" && count == 1)
+            {
+                btn1.Text = btn2.Text;
+                btn2.Text = "";
+                count = count + 1;
+            }
+            if (btn2.Text == "" && count == 1)
+            {
+                btn2.Text = btn3.Text;
+                btn3.Text = "";
+                count = count + 1;
+            }
+            if (btn4.Text == "" && count == 1)
+            {
+                btn4.Text = btn5.Text;
+                btn5.Text = "";
+                count = count + 1;
+            }
+            if (btn5.Text == "" && count == 1)
+            {
+                btn5.Text = btn6.Text;
+                btn6.Text = "";
+                count = count + 1;
+            }
+            if (btn7.Text == "" && count == 1)
+            {
+                btn7.Text = btn8.Text;
+                btn8.Text = "";
+                count = count + 1;
+            }
+            if (btn8.Text == "" && count == 1)
+            {
+                btn8.Text = btn9.Text;
+                btn9.Text = "";
+                count = count + 1;
+            }
+            win();
+        }
+
+        private void win()
+        {
+            if (btn1.Text == "1" && btn2.Text == "2" && btn3.Text == "3" && btn4.Text == "4" && btn5.Text == "5" && btn6.Text == "6" && btn7.Text == "7" && btn8.Text == "8" && btn9.Text == "")
+            {
+                timer1.Dispose();
+                gameStarted = false;
+                MessageBox.Show("Congratulations!!! You Won ");
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (!gameStarted)
+            {
+                counter = 60 * 4; // 4 Minutes
+                generateNumbers();
+                startTimer();
+                gameStarted = true;
+                btnStart.Text = "Pause game";
+            }
+            else //pause the game
+            {
+                pauseGame();
+            }
+        }
+
+        private void btnPuzzleKeyDown(object sender, KeyEventArgs e)
+        {
+            if (!gamePaused && gameStarted)  // if the game has started and the game is NOT paused stop the arrow keys input
+                switch (e.KeyCode)
+                {
+                    case Keys.Up:
+                        upAction();
+                        break;
+                    case Keys.Down:
+                        downAction();
+                        break;
+                    case Keys.Left:
+                        leftAction();
+                        break;
+                    case Keys.Right:
+                        rightAction();
+                        break;
+                }
+        }
+
+        private void btnPuzzlePreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            switch (e.KeyCode)//to handle the arrow keys
+            {
+                case Keys.Down:
+                case Keys.Up:
+                case Keys.Left:
+                case Keys.Right:
+                    e.IsInputKey = true;
+                    break;
+            }
+        }
+
+    }
+}
